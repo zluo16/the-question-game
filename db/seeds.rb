b924a25e0031a9b4dfa75f09663678a094326155
zluo16 <michael.luo@plated.com>
2018-06-06 21:44:06 -0400

    Wrote an http request to pull data from the jeopardy api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'net/http'
+
+url = URI.parse('http://jservice.io/api/random?count=500')
+req = Net::HTTP::Get.new(url.to_s)
+res = Net::HTTP.start(url.host, url.port) { | http |
+    http.request(req)
+}
+
+puts res.body
